9c5ce516895d1c79145e86ffb1a9b0e2d7644da1
subhalaxmi28 <30649036+subhalaxmi28@users.noreply.github.com>
2017-08-22 07:46:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.send("article one requested and served here.")
+
+});
+
+app.get('/article-two',function(req,res){
+    res.send("article two requested and served here.")
+
+});
+
+app.get('/article-Three',function(req,res){
+    res.send("article Three requested and served here.")
+
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
