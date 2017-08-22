783b12052b9d0d6b1f51b1d15f4400d1aa964a77
subhalaxmi28 <30649036+subhalaxmi28@users.noreply.github.com>
2017-08-22 19:53:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+     res.sendFile(path.join(__dirname, 'ui', 'article-tho.html'));
 
 });
 
