0384a2f5960aa802a530e4b7b501a8b916c73fa7
subhalaxmi28 <30649036+subhalaxmi28@users.noreply.github.com>
2017-08-22 20:52:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title: 'Article-one|Subhalaxmi Basu',
+    heading: 'Article-One',
+    date: '28th August,2017',
+    content:`<div>
+            <p>
+            this is Me
+            </p>
+             <p>
+            this is Me
+            </p>
+             <p>
+            this is Me
+            </p>
+        </div>`
+    
+};
+function createTemplate(data){
+    var title= data.title;
+    var heading= data.heading;
+    var date= data.date;
+    var content= data.content;
+var htmlTemplate=`
+    <html>
+    <head>
+       <title>
+       ${title}
+       </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+            </div>
+        <div>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        ${content}
+        </div>
+        </div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
